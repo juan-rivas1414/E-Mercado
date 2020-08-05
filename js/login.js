6c87eddbe799c765838e5f9aaa9f46b830457775
juan-rivas1414 <juan.rivas19981998@gmail.com>
2020-08-05 11:40:24 -0300

    Commits?

--- js/login.js
+++ js/login.js
@@ -2,5 +2,10 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    if(document.getElementById('submit').checked == true) { 
+        location.replace("index.html")
+     }
+     
+
 
 });
