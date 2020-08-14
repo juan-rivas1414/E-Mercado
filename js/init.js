499e28cad4bf43f7c3f0ccd48c86ff10dd65df46
juan-rivas1414 <juan.rivas19981998@gmail.com>
2020-08-14 15:47:25 -0300

    Obligatorio 1

--- js/init.js
+++ js/init.js
@@ -7,6 +7,7 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -41,6 +42,36 @@ var getJSONData = function(url){
 }
 
 
+function showCategoriesList(array){
+
+  let htmlContentToAppend = "";
+  for(let i = 0; i < array.length; i++){
+      let category = array[i];
+      htmlContentToAppend += `
+      <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + category.imgSrc + `" alt="` + category.desc + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      
+                      <h2 class="mb-1">`+ category.name +`</h2>
+                      <small class="text-muted">` + category.soldCount + ` artículos</small>
+                  </div>
+                  <br>
+                  <h6>` + category.description + `</h6>
+                  <br>
+                  <h3> ` + category.cost + " " + category.currency + `</h3>
+
+              </div>
+          </div>
+      </div>
+      `
+      document.getElementById("container p-5").innerHTML = htmlContentToAppend;//nombre de ID debe corregirse.
+    }
+}
+
 var loggeado = sessionStorage.getItem("visitado");
 if (loggeado != 1) {
   window.location.href="login.html";
@@ -50,4 +81,14 @@ if (loggeado != 1) {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
+  }); 
 });
